108a0dda20c36c7587b64dedaa311efee5ca7070
lu4ckex <lu4ckex@outlook.com>
2024-08-13 17:25:51 -0300

    Add script to calculate the area of a circle given its radius.

---
+++ circle_area.py
@@ -0,0 +1,8 @@
+print('--------------- Calculator Program v1.0 ---------------')
+print()
+
+pi = 3.14
+radius = float(input('Enter the radius of the circle: '))
+area = pi * (radius * radius)  # Area of a circle = pi x radius²
+
+print('The area of a circle with a radius of', radius, 'cm is:', area)
